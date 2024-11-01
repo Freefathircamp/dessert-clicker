5b3bbd79c1285854adafee821fbd0442d86f53a6
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-11-01 23:06:58 +0700

    init viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+
+
 android {
     namespace = "com.example.dessertclicker"
     compileSdk = 34
@@ -63,6 +65,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
 
 dependencies {
@@ -75,6 +78,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
